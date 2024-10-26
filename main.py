a9668152ef43a96fd918162138963248b97d5c5f
Kit Sum Chan <ckck16777@gmail.com>
2024-10-26 17:24:57 +0100

    better print format

--- main.py
+++ main.py
@@ -9,17 +9,17 @@ if __name__ == "__main__":
 
     question = "What programming languages are you most comfortable with, and which have you used for game development?"
     answer = interview.ask(question)
-    print(answer)
+    print(f"{question}\n\nAns:\n {answer}\n======")
 
     question = "Can you describe your experience with game engines like Unity or Minecraft’s own engine? What projects have you worked on using these tools?"
     answer = interview.ask(question)
-    print(answer)
+    print(f"{question}\n\nAns:\n {answer}\n======")
 
     question = "Can you describe your experience with game engines like Unity or Minecraft’s own engine? What projects have you worked on using these tools?"
     answer = interview.ask(question)
-    print(answer)
+    print(f"{question}\n\nAns:\n {answer}\n======")
 
     question = "What game mechanics do you think are essential for engaging gameplay? Can you provide an example from a game you enjoy?"
     answer = interview.ask(question)
-    print(answer)
+    print(f"{question}\n\nAns:\n {answer}\n======")
     
