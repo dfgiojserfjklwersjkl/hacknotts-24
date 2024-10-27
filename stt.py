3eecb8ca104f49aaa7d423201c3253f30e15888e
gldanoob <caleblolhk@gmail.com>
2024-10-27 07:30:58 +0000

    idk

--- stt.py
+++ stt.py
@@ -1,203 +1,26 @@
 
-import queue
-import re
-import keyboard
-import sys
-import time
 import os
+import re
+import sys
 
+import pyaudio
+from dotenv import load_dotenv
 from google.cloud import speech
 from inputimeout import inputimeout
 from inputimeout.inputimeout import TimeoutOccurred
-import pyaudio
-
-from dotenv import load_dotenv
 
 from config import Config
+from stream import ResumableMicrophoneStream
+
 load_dotenv()
 
 # Audio recording parameters
-STREAMING_LIMIT = 240000  # 4 minutes
 SAMPLE_RATE = Config.get("input")["sample_rate"] or 44100
 CHUNK_SIZE = int(SAMPLE_RATE / 10)  # 100ms
 
 RED = "\033[0;31m"
 GREEN = "\033[0;32m"
 YELLOW = "\033[0;33m"
-
-
-def get_current_time() -> int:
-    """Return Current Time in MS.
-
-    Returns:
-        int: Current Time in MS.
-    """
-
-    return int(round(time.time() * 1000))
-
-
-class ResumableMicrophoneStream:
-    """Opens a recording stream as a generator yielding the audio chunks."""
-
-    def __init__(
-        self,
-        rate: int,
-        chunk_size: int,
-        device_index: int = 1,
-    ) -> None:
-        """Creates a resumable microphone stream.
-
-        Args:
-        self: The class instance.
-        rate: The audio file's sampling rate.
-        chunk_size: The audio file's chunk size.
-
-        returns: None
-        """
-        self._rate = rate
-        self.chunk_size = chunk_size
-        self._num_channels = 2
-        # TODO: use mono audio
-        self._buff = queue.Queue()
-        self.closed = True
-        self.start_time = get_current_time()
-        self.restart_counter = 0
-        self.audio_input = []
-        self.last_audio_input = []
-        self.result_end_time = 0
-        self.is_final_end_time = 0
-        self.final_request_end_time = 0
-        self.bridging_offset = 0
-        self.last_transcript_was_final = False
-        self.new_stream = True
-        self._audio_interface = pyaudio.PyAudio()
-        self._audio_stream = self._audio_interface.open(
-            format=pyaudio.paInt16,
-            channels=self._num_channels,
-            rate=self._rate,
-            input=True,
-            output=False,
-            frames_per_buffer=self.chunk_size,
-            # Run the audio stream asynchronously to fill the buffer object.
-            # This is necessary so that the input device's buffer doesn't
-            # overflow while the calling thread makes network requests, etc.
-            input_device_index=device_index,
-            stream_callback=self._fill_buffer,
-        )
-
-    def __enter__(self):
-        """Opens the stream.
-
-        Args:
-        self: The class instance.
-
-        returns: None
-        """
-        self.closed = False
-        return self
-
-    def __exit__(
-        self,
-        type,
-        value,
-        traceback,
-    ):
-        """Closes the stream and releases resources.
-
-        Args:
-        self: The class instance.
-        type: The exception type.
-        value: The exception value.
-        traceback: The exception traceback.
-
-        returns: None
-        """
-        self._audio_stream.stop_stream()
-        self._audio_stream.close()
-        self.closed = True
-        # Signal the generator to terminate so that the client's
-        # streaming_recognize method will not block the process termination.
-        self._buff.put(None)
-        self._audio_interface.terminate()
-
-    def _fill_buffer(
-        self,
-        in_data,
-        *args,
-        **kwargs,
-    ):
-        """Continuously collect data from the audio stream, into the buffer.
-
-        Args:
-        self: The class instance.
-        in_data: The audio data as a bytes object.
-        args: Additional arguments.
-        kwargs: Additional arguments.
-
-        returns: None
-        """
-        self._buff.put(in_data)
-        return None, pyaudio.paContinue
-
-    def generator(self):
-        """Stream Audio from microphone to API and to local buffer
-
-        Args:
-            self: The class instance.
-
-        returns:
-            The data from the audio stream.
-        """
-        while not self.closed:
-            data = []
-
-            if self.new_stream and self.last_audio_input:
-                chunk_time = STREAMING_LIMIT / len(self.last_audio_input)
-
-                if chunk_time != 0:
-                    if self.bridging_offset < 0:
-                        self.bridging_offset = 0
-
-                    if self.bridging_offset > self.final_request_end_time:
-                        self.bridging_offset = self.final_request_end_time
-
-                    chunks_from_ms = round(
-                        (self.final_request_end_time - self.bridging_offset)
-                        / chunk_time
-                    )
-
-                    self.bridging_offset = round(
-                        (len(self.last_audio_input) - chunks_from_ms) * chunk_time
-                    )
-
-                    for i in range(chunks_from_ms, len(self.last_audio_input)):
-                        data.append(self.last_audio_input[i])
-
-                self.new_stream = False
-
-            # Use a blocking get() to ensure there's at least one chunk of
-            # data, and stop iteration if the chunk is None, indicating the
-            # end of the audio stream.
-            chunk = self._buff.get()
-            self.audio_input.append(chunk)
-
-            if chunk is None:
-                return
-            data.append(chunk)
-            # Now consume whatever other data's still buffered.
-            while True:
-                try:
-                    chunk = self._buff.get(block=False)
-
-                    if chunk is None:
-                        return
-                    data.append(chunk)
-                    self.audio_input.append(chunk)
-
-                except queue.Empty:
-                    break
-
-            yield b"".join(data)
 
 
 def listen_print_loop(responses, stream) -> str:
@@ -222,9 +45,7 @@ def listen_print_loop(responses, stream) -> str:
     sentences = [""]
 
     for response in responses:
-        if get_current_time() - stream.start_time > STREAMING_LIMIT:
-            stream.start_time = get_current_time()
-            break
+        
 
         if not response.results:
             continue
@@ -245,16 +66,13 @@ def listen_print_loop(responses, stream) -> str:
         if result.result_end_time.microseconds:
             result_micros = result.result_end_time.microseconds
 
-        stream.result_end_time = int((result_seconds * 1000) + (result_micros / 1000))
+        stream.result_end_time = int(
+            (result_seconds * 1000) + (result_micros / 1000))
 
-        corrected_time = (
-            stream.result_end_time
-            - stream.bridging_offset
-            + (STREAMING_LIMIT * stream.restart_counter)
-        )
+        corrected_time = stream.corrected_time()
+        
         # Display interim results, but with a carriage return at the end of the
         # line, so subsequent lines will overwrite them.
-
 
         if result.is_final:
             sys.stdout.write(str(corrected_time) + ": " + transcript + "\n")
@@ -279,7 +97,6 @@ def listen_print_loop(responses, stream) -> str:
 
             stream.last_transcript_was_final = False
 
-
         try:
             inputimeout(prompt='', timeout=0.1)
             return ''.join(sentences)
@@ -287,11 +104,7 @@ def listen_print_loop(responses, stream) -> str:
         except TimeoutOccurred as e:
             pass
 
-
     return ""
-
-        
-
 
 
 def main(language: str, device_index: int) -> str:
@@ -309,7 +122,8 @@ def main(language: str, device_index: int) -> str:
         config=config, interim_results=True
     )
 
-    mic_manager = ResumableMicrophoneStream(SAMPLE_RATE, CHUNK_SIZE, device_index)
+    mic_manager = ResumableMicrophoneStream(
+        SAMPLE_RATE, CHUNK_SIZE, device_index)
     print(mic_manager.chunk_size)
     sys.stdout.write(YELLOW)
     sys.stdout.write('\nListening, say "Quit" or "Exit" to stop.\n\n')
@@ -322,7 +136,7 @@ def main(language: str, device_index: int) -> str:
         while not stream.closed:
             sys.stdout.write(YELLOW)
             sys.stdout.write(
-                "\n" + str(STREAMING_LIMIT * stream.restart_counter) + ": NEW REQUEST\n"
+                "\n" + str("NEW REQUEST\n")
             )
 
             stream.audio_input = []
@@ -335,14 +149,10 @@ def main(language: str, device_index: int) -> str:
 
             responses = client.streaming_recognize(streaming_config, requests)
 
-
             # Now, put the transcription responses to use.
             transcript_output = listen_print_loop(responses, stream)
 
-
             return transcript_output
-
-
 
             if stream.result_end_time > 0:
                 stream.final_request_end_time = stream.is_final_end_time
@@ -356,18 +166,18 @@ def main(language: str, device_index: int) -> str:
                 sys.stdout.write("\n")
             stream.new_stream = True
 
-
-        else: 
+        else:
             return ""
+
 
 if __name__ == "__main__":
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'google-api-key.json'
 
     pya = pyaudio.PyAudio()
     info = pya.get_host_api_info_by_index(0)
-    numdevices = info.get('deviceCount') 
+    numdevices = info.get('deviceCount')
     device_index = None
-    print ( "Available devices:\n")
+    print("Available devices:\n")
     for i in range(pya.get_device_count()):
         devinfo = pya.get_device_info_by_index(i)
         if devinfo['name'] == 'pulse':
